9c6d87cae04d1c0f937da496bf225ec96fcdfe03
geethika2208 <venkatageethika.geethu@gmail.com>
2026-06-21 18:05:02 +0530

    Connect frontend to Render API

--- frontend/src/ProductList.js
+++ frontend/src/ProductList.js
@@ -5,7 +5,7 @@ function ProductList() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8000/api/products/")
+    fetch("https://inventory-backend-88x9.onrender.com/api/products/")
       .then((response) => response.json())
       .then((data) => setProducts(data))
       .catch((error) => console.error("Error fetching products:", error));
